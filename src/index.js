db5553e4422109803006fcbc83f44150ebae48fd
Mithat Kuş <mithatkus@gmail.com>
2026-01-28 01:42:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,31 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-```
-
-**`src/App.js`**
-Artifact'teki tüm kodu buraya kopyalayın (import satırlarından başlayarak export default QRCodeGenerator; satırına kadar).
-
-**`.gitignore`**
-```
-# dependencies
-/node_modules
-/.pnp
-.pnp.js
-
-# testing
-/coverage
-
-# production
-/build
-
-# misc
-.DS_Store
-.env.local
-.env.development.local
-.env.test.local
-.env.production.local
-
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
